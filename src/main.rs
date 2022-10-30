a4f60d5c860d68dae6a6fd50d7aa65ed6e2dcaad
Maxim <defilak@mail.ru>
2022-10-31 02:01:56 +0300

    add help

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use crate::child_proc::{run_processes, ChildProcess};
-use crate::logger::log;
 use std::env;
 use std::{
-    sync::{atomic::{AtomicBool, Ordering}, Arc},
-    thread::{self, JoinHandle},
+    sync::{atomic::AtomicBool, Arc},
+    thread,
     time::Duration,
 };
+
+use crate::child_proc::{run_processes, ChildProcess};
+use crate::logger::log;
 
 mod child_proc;
 mod child_service;
@@ -24,10 +25,6 @@ pub fn main() {
 
 #[cfg(windows)]
 fn main() -> windows_service::Result<()> {
-
-    use windows_service::service::ServiceState;
-
-
     let args: Vec<String> = env::args().collect();
 
     match args.get(1) {
@@ -59,7 +56,7 @@ fn main() -> windows_service::Result<()> {
             }
             "runservice" => {
                 match monitor_service::run() {
-                    Err(err) => log!("{:?}",&err),
+                    Err(err) => log!("{:?}", &err),
                     _ => (),
                 }
                 Ok(())
@@ -67,51 +64,8 @@ fn main() -> windows_service::Result<()> {
             _ => Ok(()),
         },
         None => {
-            let mut threads = Vec::<JoinHandle<()>>::new();
-            let need_exit = Arc::new(AtomicBool::new(false));
-            let child_service1 = child_service::ChildServiceControl::new(child_service::APACHE_SERVICE_NAME);
-            if child_service1.is_ok() {
-                let mut proc = child_service1.unwrap();
-                let exit_flag = need_exit.clone();
-
-                log!("Starting {}", proc.name);
-
-                threads.push(thread::spawn(move || {
-                    match proc.start() {
-                        Ok(_) => log!("{} started", &proc.name),
-                        Err(err) => log!("{:?}",&err),
-                    };
-
-                    loop {
-                        if exit_flag.load(Ordering::Relaxed) == true {
-                            log!("Stopping: {:?}", &proc.name);
-                            match proc.stop() {
-                                Ok(_) => log!("{} stopped", &proc.name),
-                                Err(err) => log!("{:?}", &err),
-                            };
-                            break;
-                        }
-
-                        match proc.status() {
-                            Ok(status) => {
-                                if status.current_state != ServiceState::Running {
-                                    log!("Restarting service {}: {:?}", &proc.name, status.current_state);
-                                    match proc.start() {
-                                        Ok(()) => log!("Service {} restarted",&proc.name),
-                                        Err(err) => log!("Can't restart service {}: {}", &proc.name, err)
-                                    };
-                                }
-                            },
-                            Err(err) => log!("Can't get status for service {}: {}", &proc.name, err)
-                        };
-
-                        thread::sleep(Duration::from_millis(100));
-                    }
-                }));
-            }
-            while !threads.iter().all(|t| t.is_finished()) {
-                thread::sleep(Duration::from_millis(100));
-            }
+            println!("Using: servicers.exe <command>");
+            println!("Available commands: install, uninstall, start, stop, run, runservice");
 
             Ok(())
         }
