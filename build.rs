3fd8d8df2fa0e25e2d8fff7716ff98da23ee7706
Maxim <defilak@mail.ru>
2022-10-31 02:01:45 +0300

    win metadata

--- build.rs
+++ build.rs
@@ -3,7 +3,13 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("favicon.ico");
+        res.set_icon("favicon.ico")
+        .set("InternalName", env!("CARGO_PKG_NAME"))
+        .set("OriginalFilename", env!("CARGO_PKG_NAME"))
+        .set("FileDescription", env!("CARGO_PKG_DESCRIPTION"))
+        .set("LegalCopyright", env!("CARGO_PKG_LICENSE"))
+        .set("ProductVersion", env!("CARGO_PKG_VERSION"));
+
         res.compile().unwrap();
     }
 }
